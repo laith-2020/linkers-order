2291e166057c3b2a61124c8939dd3d17079a4b5d
laith <mr.layth_2011@hotmail.com>
2020-08-21 00:25:28 +0300

    add api

--- public/it-next/js/app.js
+++ public/it-next/js/app.js
@@ -76,3 +76,35 @@ function showSlides() {
     dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+
+
+
+///// module 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+        console.log('lolo')
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// $('#myBtn').onclick(() => {
+
+//     console.log('Soso');
+// })
+
+
+$("#myBtn").click(function() {
+    // alert("The paragraph was clicked.");
+    console.log('sososososo');
+});
---
+++ server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+require("dotenv").config();
+const express = require("express");
+const superagent = require("superagent");
+const { get } = require("superagent");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+
+
+    let url = `http://orderlink.linkers.io:8096/Landing/GetMenu/?storeManagerId=5e97b15d02c81e1547ca5c8d`;
+    superagent.get(url)
+        .then((result) => {
+
+            var array = [];
+            let resultData = result.body.map(item => {
+                array.push(item.category); //To store the keywords for the options
+                array = array.filter(function(item, index, inputArray) { //Filter to delet the repeated keywords;
+                    return inputArray.indexOf(item) == index;
+                });
+                return new Category(item);
+            })
+            console.log('soso', array);
+            let resultOBJ = new Restorant(result.body);
+            res.render("index", { resultKey: resultOBJ, categoryKey: array, productKey: resultData });
+        })
+})
+
+function Restorant(data) {
+    this.shopName = data[0].availableBranches[0].storeDetails.shopName;
+    this.city = data[0].availableBranches[0].storeDetails.city;
+    this.district = data[0].availableBranches[0].storeDetails.district;
+    this.deliveryTime = data[0].availableBranches[0].storeDetails.deliveryTime;
+    this.minimumOrder = data[0].availableBranches[0].storeDetails.minimumOrder;
+    this.deliveryFees = data[0].availableBranches[0].storeDetails.deliveryFees;
+    this.isEnabled = data[0].availableBranches[0].storeDetails.isEnabled;
+    this.isClosed = data[0].availableBranches[0].storeDetails.isClosed;
+    this.category = data.category;
+
+}
+
+function Category(data) {
+    this.category = data.category;
+    this.productName = data.productName;
+}
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
